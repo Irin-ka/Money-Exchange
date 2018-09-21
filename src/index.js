de90a98215278157c3fc6e00c6d42c5a788ba8bd
ira <irin-ka@inbox.ru>
2018-09-21 15:29:58 +0300

    money

--- src/index.js
+++ src/index.js
@@ -1,5 +1,48 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    console.log(currency); 
+    var obj = {}; 
+    var x,y
+    
+    if (currency <=0){
+    obj = {};
+    }
+    else if (currency >10000){
+    obj = {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    else{
+    if (currency >=50) {
+    x = Math.floor(currency / 50);
+    obj.H = x;
+    }
+    y = currency%50
+    currency = y;
+    if(currency >=25) {
+    var x = Math.floor(currency / 25);
+    obj.Q = x;
+    }
+    y = currency%25
+    currency = y;
+    if(currency >=10) {
+    var x = Math.floor(currency / 10);
+    obj.D = x;
+    }
+    y = currency%10
+    currency = y;
+    if(currency >=5) {
+    var x = Math.floor(currency / 5);
+    obj.N = x;
+    }
+    y = currency%5
+    currency = y;
+    if(currency >=1) {
+    var x = currency / 1;
+    obj.P = x;
+    }
+    }//else...
+    console.log(obj);
+    return obj;
+    }
+    
+    
+
